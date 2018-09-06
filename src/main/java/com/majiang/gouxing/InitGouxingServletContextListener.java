17df6388b7fba2fdaa51c466f1d3312ab65cded9
Administrator <Administrator@WIN-OKH646EVNPT>
2018-09-06 11:02:41 +0800

    内存共享做法改变

--- src/main/java/com/majiang/gouxing/InitGouxingServletContextListener.java
+++ src/main/java/com/majiang/gouxing/InitGouxingServletContextListener.java
@@ -1,19 +1,17 @@
 package com.majiang.gouxing;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
 import com.dml.majiang.player.shoupai.gouxing.GouXingCalculator;
+import com.dml.majiang.player.shoupai.gouxing.GouXingCalculatorHelper;
 
 @WebListener
 public class InitGouxingServletContextListener implements ServletContextListener {
 
 	public void contextInitialized(ServletContextEvent sce) {
-		ServletContext sc = sce.getServletContext();
-		sc.setAttribute("17_3", new GouXingCalculator(17, 3));
-		sc.setAttribute("14_4", new GouXingCalculator(14, 4));
+		GouXingCalculatorHelper.gouXingCalculator = new GouXingCalculator(17, 4);
 	}
 
 	public void contextDestroyed(ServletContextEvent sce) {
